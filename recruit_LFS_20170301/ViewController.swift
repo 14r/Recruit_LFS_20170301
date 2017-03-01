77d69119c6204e8947747ceabbaaab37e0c393c4
14r <73marly@gmail.com>
2017-03-01 10:38:10 +0900

    Initial Commit

---
+++ recruit_LFS_20170301/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recruit_LFS_20170301
+//
+//  Created by Arisa on 2017/03/01.
+//  Copyright © 2017年 Arisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
